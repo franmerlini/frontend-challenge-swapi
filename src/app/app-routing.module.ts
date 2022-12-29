cb582bf25fd3a1fee7564e8f844cf792e56069e0
Francisco <merlinifrancisco@gmail.com>
2022-12-29 18:44:58 -0300

    feat: Add core module and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    path: 'home',
-    loadChildren: () => import('./modules').then((m) => m.HomeModule),
-  },
-  {
-    path: 'movies',
-    loadChildren: () => import('./modules').then((m) => m.MoviesModule),
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./modules').then((m) => m.CharactersModule),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadChildren: () => import('./modules').then((m) => m.HomeModule),
+      },
+      {
+        path: 'movies',
+        loadChildren: () => import('./modules').then((m) => m.MoviesModule),
+      },
+      {
+        path: 'characters',
+        loadChildren: () => import('./modules').then((m) => m.CharactersModule),
+      },
+    ],
   },
   // 404 Not Found
   { path: '**', redirectTo: 'home' },
