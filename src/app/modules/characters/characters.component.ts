24a616e71f06198f989ba06bf178d21505156dad
Francisco <merlinifrancisco@gmail.com>
2022-12-31 23:39:06 -0300

    feat: Add filters to characters table

--- src/app/modules/characters/characters.component.ts
+++ src/app/modules/characters/characters.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { map, Observable, Subscription } from 'rxjs';
+import { combineLatest, map, Observable, startWith } from 'rxjs';
 import { Character, Movie } from 'src/app/models/domain';
 import { State } from 'src/app/models/state';
 import { CharactersActions, MoviesActions } from 'src/app/state/actions';
@@ -21,18 +22,55 @@ export class CharactersComponent implements OnInit {
   public movies$: Observable<Movie[]> = this.store.select(
     MoviesSelectors.selectMovies
   );
-  public moviesSubscription!: Subscription;
+  public form!: FormGroup;
+  public filteredCharacters$!: Observable<Character[]>;
 
-  constructor(private readonly store: Store<State>) {}
+  constructor(
+    private readonly store: Store<State>,
+    private readonly fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.fetchData();
+    this.createForm();
+    this.startFiltering();
+  }
+
+  private fetchData(): void {
     this.store.dispatch(CharactersActions.getData());
     this.store.dispatch(MoviesActions.getData());
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      filter: [''],
+    });
+  }
+
+  private startFiltering(): void {
+    const filter$: Observable<string> = this.filter.valueChanges.pipe(
+      startWith('')
+    );
+    this.filteredCharacters$ = combineLatest([this.characters$, filter$]).pipe(
+      map(([characters, filter]) => {
+        return characters.filter((character) => {
+          return (
+            character.name.toLowerCase().startsWith(filter.toLowerCase()) ||
+            character.gender.toLowerCase().startsWith(filter.toLowerCase()) ||
+            character.eye_color.toLowerCase().startsWith(filter.toLowerCase())
+          );
+        });
+      })
+    );
   }
 
   public getMovie(url: string): Observable<Movie | undefined> {
     return this.movies$.pipe(
       map((movies) => movies.find((movie) => movie.url === url))
     );
+  }
+
+  get filter(): AbstractControl {
+    return this.form.get('filter') as AbstractControl;
   }
 }
--- src/app/modules/characters/characters.module.ts
+++ src/app/modules/characters/characters.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersComponent } from './characters.component';
 
 @NgModule({
   declarations: [CharactersComponent],
-  imports: [CommonModule, CharactersRoutingModule],
+  imports: [CommonModule, CharactersRoutingModule, SharedModule],
 })
 export class CharactersModule {}
