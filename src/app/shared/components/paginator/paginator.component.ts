e25b23f67fc9ebbb1539d36c4c7f2797e86046d5
Francisco <merlinifrancisco@gmail.com>
2023-01-03 13:41:20 -0300

    feat: Create paginator component

---
+++ src/app/shared/components/paginator/paginator.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator',
+  templateUrl: './paginator.component.html',
+  styleUrls: ['./paginator.component.scss'],
+})
+export class PaginatorComponent {
+  public currentPage = 0;
+  public totalPages = 0;
+  public itemsPerPageOptions = [
+    { value: 5, label: '5' },
+    { value: 10, label: '10' },
+    { value: 20, label: '20' },
+    { value: 50, label: '50' },
+  ];
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PaginatorComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [ReactiveFormsModule],
+  exports: [ReactiveFormsModule, PaginatorComponent],
 })
 export class SharedModule {}
