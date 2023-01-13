79ecf71f6eb514362d39ae19fd4c025ec0cc7f0c
Francisco <merlinifrancisco@gmail.com>
2023-01-13 17:41:19 -0300

    style: Fix responsive in navbar component

--- src/app/core/layout/navbar/navbar.component.ts
+++ src/app/core/layout/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
 
+  public toggle(): void {
+    this.navbarCollapse.nativeElement.classList.toggle('responsive');
+  }
 }
